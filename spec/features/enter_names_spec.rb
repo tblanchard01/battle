26b21786b36e0035d371dac371d4997ebfe12ae4
Tim Blanchard <tim.blanchard@hotmail.co.uk>
2018-07-26 19:53:09 +0100

    added web helpers

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,7 @@
 feature 'Enter names' do 
   scenario 'submitting names' do 
-visit('/')
-fill_in :player_1_name, with: 'Dave'
-fill_in :player_2_name, with: 'Mittens'
-click_button 'Let battle commence!'
+    sign_in_and_play 
 expect(page).to have_content 'Dave vs Mittens'   
+expect(page).to have_content 'Mittens: 60HP' 
 end 
 end 
